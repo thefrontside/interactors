6f6ecd57a6b520b032c66b5c9ad902c9e414e502
Elrick Ryan <elrick@frontside.com>
2021-03-25 16:28:34 -0400

    Export Fab Interactor

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-export { Button } from "@bigtest/interactor";
-export { MaterialCheckbox as CheckBox } from "./checkbox";
+import * as Bigtest from '@bigtest/interactor';
+
+export { Bigtest };
+export { HTML, including, matching, and, or, not, some, every } from '@bigtest/interactor';
+
 export { Body } from "./body";
+export { MaterialCheckbox as CheckBox } from "./checkbox";
+export { Button } from "@bigtest/interactor";
+export { default as Fab } from './fab';
