c646cacf14ca3a3edf6b456605d98614a795f607
Elrick Ryan <elrick@frontside.com>
2021-03-29 20:53:52 -0400

    Use mui selector to grab fab

--- src/fab.ts
+++ src/fab.ts
@@ -1,6 +1,7 @@
 import { Button } from '@bigtest/interactor';
 
 export default Button.extend('button')
+  .selector('button.MuiFab-root')
   .filters({
     ariaLabel: (el) => el.getAttribute("aria-label"),
     svgIcon: (el) => el.querySelector('svg')?.classList.contains('MuiSvgIcon-root')
