e7c13107cbe1669ca68bf30e87e997ae9617672f
Elrick Ryan <elrick@frontside.com>
2021-04-12 15:35:50 -0400

    Change name to slider from sliderthumb

--- test/slider.test.tsx
+++ test/slider.test.tsx
@@ -1,9 +1,9 @@
 import { Page, test } from 'bigtest';
-import { SliderThumb as Interactor, including } from '../src/index';
+import { Slider as Interactor, including } from '../src/index';
 import { Slider as MuiSlider, Typography } from '@material-ui/core'
 import { render } from './helpers';
 
-const sliderThumb = Interactor;
+const slider = Interactor;
 
 export default test("slider")
   .step(Page.visit("/"))
@@ -12,12 +12,12 @@ export default test("slider")
       await render(<MuiSlider defaultValue={20}/>)
     })
     .assertion("we have a slider", async() => {
-      sliderThumb().exists()
+      slider().exists()
     })
-    .assertion(sliderThumb({ index: "0"}).exists())
+    .assertion(slider({ index: "0"}).exists())
   )
-  .child("another test", (test) => test
-    .step("render", async() => {
+  .child("slider with two thumbs", (test) => test
+    .step("render slider with two thumbs", async() => {
       const marks = [
         {
           value: 0,
@@ -59,32 +59,63 @@ export default test("slider")
       })
       .child("can control slider thumb with keyboard", (test) => test
         .step('key thumb right', async() => {
-          const thumb1 = sliderThumb({ index: "0"})
+          const thumb1 = slider({ index: "0"})
           await thumb1.keyboard("ArrowRight");
         })
-        .assertion(sliderThumb({ index: "0"}).has({ ariaValueNow: including('30')}))
+        .assertion(slider({ index: "0"}).has({ ariaValueNow: including('30')}))
         .child("key thumb left", (test) => test
         .step('key thumb left', async() => {
-          const thumb1 = sliderThumb({ index: "0"})
+          const thumb1 = slider({ index: "0"})
           await thumb1.keyboard("ArrowLeft");
           })
-          .assertion(sliderThumb({ index: "0"}).has({ ariaValueNow: including('20')}))
+          .assertion(slider({ index: "0"}).has({ ariaValueNow: including('20')}))
         )
       )
       .child("can control slider thumb with mouse", (test) => test
         .step('Mouse click right', async() => {
-          const thumb1 = sliderThumb({ index: "0"})
+          const thumb1 = slider({ index: "0"})
           await thumb1.mouseDown(127)
           await thumb1.mouseUp(127)
         })
-        .assertion(sliderThumb({ index: "0"}).has({ ariaValueNow: including('10')}))
+        .assertion(slider({ index: "0"}).has({ ariaValueNow: including('10')}))
         .child("mouse click left", (test) => test
         .step('click mouse', async() => {
-          const thumb1 = sliderThumb({ index: "0"})
+          const thumb1 = slider({ index: "0"})
           await thumb1.mouseDown(200)
           await thumb1.mouseUp(200)
           })
-          .assertion(sliderThumb({ index: "0"}).has({ ariaValueNow: including('20')}))
+          .assertion(slider({ index: "0"}).has({ ariaValueNow: including('20')}))
+        )
+        .child("click second thumb", (test) => test
+          .step("Something", async() => {
+            const thumb2 = slider({ index: "1" })
+            await thumb2.mouseDown(900)
+            await thumb2.mouseUp(900)
+          })
+          .assertion(slider({ index: "1"}).has({ ariaValueNow: including('100')}))
         )
       )
+  )
+  .child("slider with one thumb", (test) => test
+    .step("render slider with one thumb", async() => {
+      await render(
+        <div style={{ width: '50em', padding: '1em'}}>
+          <Typography id="discrete-slider-always" gutterBottom>
+            Always visible
+          </Typography>
+          <MuiSlider
+            defaultValue={20}
+            // getAriaValueText={valuetext}
+            aria-labelledby="discrete-slider-always"
+            // step={10}
+            // marks={marks}
+            valueLabelDisplay="on"
+          />
+        </div>)
+    })
+    .child("click thumb", (test) => test
+      .step('click', async() => {
+        const thumb2 = slider({ index: "1" })
+      })
+    )
   )
