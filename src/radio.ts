33e2d884a9e066b2f7ebd35246cf1859d531de27
Elrick Ryan <elrick@frontside.com>
2021-05-18 09:39:49 -0400

    Target mui class

--- src/radio.ts
+++ src/radio.ts
@@ -1,3 +1,10 @@
-import { RadioButton } from '@bigtest/interactor';
+import { RadioButton, isVisible } from '@bigtest/interactor';
 
 export default RadioButton.extend('radio')
+  .selector('.MuiRadio-root input[type=radio]')
+  .filters({
+    visible: {
+      apply: (el) => isVisible(el.closest('.MuiRadio-root')!),
+      default: true
+    }
+  })
