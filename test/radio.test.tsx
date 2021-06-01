3403dc27b9cf99fdb6f8b6ed744dbe2ee0371e5c
Elrick Ryan <elrick@frontside.com>
2021-06-01 14:20:14 -0400

    Create and use renderstep

--- test/radio.test.tsx
+++ test/radio.test.tsx
@@ -1,16 +1,15 @@
 import { test, Page } from 'bigtest';
 import { Radio as Interactor, or} from '../src/index'
 import { Radio as MuiRadio } from '@material-ui/core';
-import { render } from './helpers';
+import { render, createRenderStep } from './helpers';
 
 const radio = Interactor();
+const renderRadioButton = createRenderStep(MuiRadio)
 
 export default test("Radio")
   .step(Page.visit("/"))
   .child("can render radio button", (test) => test
-    .step("render radio button", async() => {
-      await render(<MuiRadio/>)
-    })
+    .step(renderRadioButton())
     .assertion(radio.exists())
     .assertion(radio.is({ checked: false }))
   )
