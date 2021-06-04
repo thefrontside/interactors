dbc5f0cdca7b19459d72ab5d71819fa88cc7e701
wKich <w@kich.dev>
2021-06-04 08:08:49 +0200

    add snackbar interactor

---
+++ src/snackbar.ts
@@ -0,0 +1,9 @@
+import { HTML } from "bigtest";
+import { isHTMLElement } from "./helpers";
+
+export const Snackbar = HTML.extend("MUI Snackbar")
+  .selector(".MuiSnackbar-root")
+  .locator((element) => {
+    const messageElement = element.querySelector(".MuiSnackbarContent-message");
+    return isHTMLElement(messageElement) ? messageElement.innerText : "";
+  });
