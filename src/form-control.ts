a26282e61f4a812c1d9b1080d6bcf5e13ba127ce
wKich <w@kich.dev>
2021-06-09 11:33:58 +0200

    add description filter for form-control interactor

--- src/form-control.ts
+++ src/form-control.ts
@@ -1,9 +1,18 @@
 import { HTML } from "bigtest";
 import { isHTMLElement } from "./helpers";
 
+function getLabelElement(root: HTMLElement) {
+  const label = root.querySelector("label.MuiFormLabel-root");
+  return isHTMLElement(label) ? label : null;
+}
+
 export const FormControl = HTML.extend("MUI Form Control")
   .selector(".MuiFormControl-root")
-  .locator((element) => {
-    const label = element.querySelector("label.MuiFormLabel-root");
-    return isHTMLElement(label) ? label.innerText : "";
+  .locator((element) => getLabelElement(element)?.innerText ?? "")
+  .filters({
+    valid: (element) => !getLabelElement(element)?.classList.contains("Mui-error"),
+    description: (element) => {
+      const descriptionElement = element?.lastElementChild;
+      return isHTMLElement(descriptionElement) ? descriptionElement.innerText : "";
+    },
   });
