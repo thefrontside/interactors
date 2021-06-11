e0ca5418150633b139c08cc64252933e7800d8e4
wKich <w@kich.dev>
2021-06-11 08:36:27 +0200

    add dialog interactor

---
+++ src/dialog.ts
@@ -0,0 +1,17 @@
+import { HTML } from "bigtest";
+import { isHTMLElement } from "./helpers";
+
+export const Dialog = HTML.extend("MUI Dialog")
+  .selector('.MuiDialog-root[role="presentation"]:not([aria-hidden="true"])')
+  .locator((element) => {
+    const labelId = element.querySelector('.MuiDialog-paper[role="dialog"]')?.getAttribute("aria-labelledby");
+    const titleElement = labelId ? element.querySelector(`#${labelId}`) : element.querySelector(".MuiDialogTitle-root");
+    return isHTMLElement(titleElement) ? titleElement.innerText : "";
+  })
+  .actions({
+    close: ({ perform }) =>
+      perform((element) => {
+        const backdrop = element.querySelector(".MuiBackdrop-root");
+        if (isHTMLElement(backdrop)) backdrop.click();
+      }),
+  });
