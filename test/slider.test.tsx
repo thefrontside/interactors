504bd6344c1d1489b0fdcaa43cea58b58e66a090
wKich <w@kich.dev>
2021-06-30 15:45:09 +0200

    refactor slider tests

--- test/slider.test.tsx
+++ test/slider.test.tsx
@@ -35,55 +35,69 @@ export default test("Slider")
   .child('Slider with one thumb', (test) => test
     .child('default', (test) => test
       .step(renderSlider())
-      .assertion(slider.exists())
-      .assertion(slider.has({ orientation: 'horizontal' }))
-      .assertion(slider.has({ value: 0 }))
-      .assertion(slider.has({ textValue: '0°C' }))
-      .assertion(slider.has({ minValue: -100 }))
-      .assertion(slider.has({ maxValue: 200 }))
-      .assertion(slider.is({ horizontal: true }))
-      .assertion(slider.is({ vertical: false }))
-      .assertion(slider.is({ min: false }))
-      .assertion(slider.is({ max: false }))
-      .assertion(slider.find(Thumb()).has({ index: 0 }))
-      .assertion(slider.find(Thumb()).has({ value: 0 }))
-      .assertion(slider.find(Thumb()).has({ textValue: '0°C' }))
-      .assertion(slider.find(Thumb('1st')).exists())
-      .assertion(slider.find(Thumb({ disabled: false })).exists())
-      .assertion(Slider('1st').exists())
-      .assertion(Slider({ disabled: false }).exists())
-      .assertion(Slider('My Slider').absent())
+      .assertion(
+        slider.exists(),
+        slider.has({ orientation: 'horizontal' }),
+        slider.has({ value: 0 }),
+        slider.has({ textValue: '0°C' }),
+        slider.has({ minValue: -100 }),
+        slider.has({ maxValue: 200 }),
+        slider.is({ horizontal: true }),
+        slider.is({ vertical: false }),
+        slider.is({ min: false }),
+        slider.is({ max: false }),
+        slider.find(Thumb()).has({ index: 0 }),
+        slider.find(Thumb()).has({ value: 0 }),
+        slider.find(Thumb()).has({ textValue: '0°C' }),
+        slider.find(Thumb('1st')).exists(),
+        slider.find(Thumb({ disabled: false })).exists(),
+        Slider('1st').exists(),
+        Slider({ disabled: false }).exists(),
+        Slider('My Slider').absent()
+      )
       .child('decrease', (test) => test
         .step(slider.decrease())
-        .assertion(slider.has({ value: -1 }))
-        .assertion(slider.has({ textValue: '-1°C' }))
+        .assertion(
+          slider.has({ value: -1 }),
+          slider.has({ textValue: '-1°C' })
+        )
       )
       .child('decrease by 10', (test) => test
         .step(slider.decrease(10))
-        .assertion(slider.has({ value: -10 }))
-        .assertion(slider.has({ textValue: '-10°C' }))
+        .assertion(
+          slider.has({ value: -10 }),
+          slider.has({ textValue: '-10°C' })
+        )
       )
       .child('decrease by 200', (test) => test
         .step(slider.decrease(200))
-        .assertion(slider.has({ value: -100 }))
-        .assertion(slider.has({ textValue: '-100°C' }))
-        .assertion(slider.is({ min: true }))
+        .assertion(
+          slider.has({ value: -100 }),
+          slider.has({ textValue: '-100°C' }),
+          slider.is({ min: true })
+        )
       )
       .child('increase', (test) => test
         .step(slider.increase())
-        .assertion(slider.has({ value: 1 }))
-        .assertion(slider.has({ textValue: '1°C' }))
+        .assertion(
+          slider.has({ value: 1 }),
+          slider.has({ textValue: '1°C' })
+        )
       )
       .child('increase by 10', (test) => test
         .step(slider.increase(10))
-        .assertion(slider.has({ value: 10 }))
-        .assertion(slider.has({ textValue: '10°C' }))
+        .assertion(
+          slider.has({ value: 10 }),
+          slider.has({ textValue: '10°C' })
+        )
       )
       .child('increase by 300', (test) => test
         .step(slider.increase(300))
-        .assertion(slider.has({ value: 200 }))
-        .assertion(slider.has({ textValue: '200°C' }))
-        .assertion(slider.is({ max: true }))
+        .assertion(
+          slider.has({ value: 200 }),
+          slider.has({ textValue: '200°C' }),
+          slider.is({ max: true })
+        )
       )
       .child('setMin', (test) => test
         .step(slider.setMin())
@@ -95,7 +109,8 @@ export default test("Slider")
       )
       .child('setValue', (test) => test
         .step(slider.setValue(100))
-        .assertion(slider.has({ value: 100 }))
+        // NOTE: This is expected behavior because the slide's step width is less than 1 px
+        .assertion(slider.has({ value: 101 }))
       )
       .child('setValue below min', (test) => test
         .step(slider.setValue(-200))
@@ -115,16 +130,16 @@ export default test("Slider")
       .assertion(Slider('Slider Label').exists())
     )
     .child('without getAriaLabel', (test) => test
-      .step(
-        renderSlider({ getAriaLabel: undefined })
-      )
+      .step(renderSlider({ getAriaLabel: undefined }))
       .assertion(slider.find(Thumb('0°C')).exists())
     )
     .child('orientation="vertical"', (test) => test
       .step(renderSlider({ orientation: 'vertical' }))
-      .assertion(slider.has({ orientation: 'vertical' }))
-      .assertion(slider.is({ horizontal: false }))
-      .assertion(slider.is({ vertical: true }))
+      .assertion(
+        slider.has({ orientation: 'vertical' }),
+        slider.is({ horizontal: false }),
+        slider.is({ vertical: true })
+      )
       .child('decrease', (test) => test
         .step(slider.decrease())
         .assertion(slider.has({ value: -1 }))
@@ -135,8 +150,10 @@ export default test("Slider")
       )
       .child('decrease by 200', (test) => test
         .step(slider.decrease(200))
-        .assertion(slider.has({ value: -100 }))
-        .assertion(slider.is({ min: true }))
+        .assertion(
+          slider.has({ value: -100 }),
+          slider.is({ min: true })
+        )
       )
       .child('increase', (test) => test
         .step(slider.increase())
@@ -148,8 +165,10 @@ export default test("Slider")
       )
       .child('increase by 300', (test) => test
         .step(slider.increase(300))
-        .assertion(slider.has({ value: 200 }))
-        .assertion(slider.is({ max: true }))
+        .assertion(
+          slider.has({ value: 200 }),
+          slider.is({ max: true })
+        )
       )
       .child('setMin', (test) => test
         .step(slider.setMin())
@@ -191,8 +210,10 @@ export default test("Slider")
       )
       .child('decrease by 10', (test) => test
         .step(slider.decrease(10))
-        .assertion(slider.has({ value: -100 }))
-        .assertion(slider.is({ min: true }))
+        .assertion(
+          slider.has({ value: -100 }),
+          slider.is({ min: true })
+        )
       )
       .child('increase', (test) => test
         .step(slider.increase())
@@ -204,8 +225,10 @@ export default test("Slider")
       )
       .child('increase by 10', (test) => test
         .step(slider.increase(10))
-        .assertion(slider.has({ value: 200 }))
-        .assertion(slider.is({ max: true }))
+        .assertion(
+          slider.has({ value: 200 }),
+          slider.is({ max: true })
+        )
       )
       .child('setMin', (test) => test
         .step(slider.setMin())
@@ -217,8 +240,7 @@ export default test("Slider")
       )
       .child('setValue', (test) => test
         .step(slider.setValue(100))
-        // NOTE: This is expected behavior because the slide's step width is less than 1 px
-        .assertion(slider.has({ value: 101 }))
+        .assertion(slider.has({ value: 100 }))
       )
       .child('setValue a little below the mark', (test) => test
         .step(slider.setValue(90))
@@ -239,8 +261,10 @@ export default test("Slider")
     )
     .child('disabled={true}', (test) => test
       .step(renderSlider({ disabled: true }))
-      .assertion(slider.absent())
-      .assertion(disabledSlider.exists())
+      .assertion(
+        slider.absent(),
+        disabledSlider.exists()
+      )
       .child('decrease', (test) => test
         .step(disabledThumb.decrease())
         .assertion(Slider({ disabled: true, value: 0 }).exists())
@@ -248,16 +272,16 @@ export default test("Slider")
       .child('increase', (test) => test
         .step(disabledThumb.increase())
         .assertion(Slider({ disabled: true, value: 0 }).exists())
-        )
-        .child('setMin', (test) => test
+      )
+      .child('setMin', (test) => test
         .step(disabledThumb.setMin())
         .assertion(Slider({ disabled: true, min: false }).exists())
-        )
-        .child('setMax', (test) => test
+      )
+      .child('setMax', (test) => test
         .step(disabledThumb.setMax())
         .assertion(Slider({ disabled: true, max: false }).exists())
-        )
-        .child('setValue', (test) => test
+      )
+      .child('setValue', (test) => test
         .step(disabledThumb.setValue(100))
         .assertion(Slider({ disabled: true, value: 0 }).exists())
       )
@@ -266,88 +290,114 @@ export default test("Slider")
   .child('Slider with multiple thumbs', (test) => test
     .child('default', (test) => test
       .step(renderSlider({ defaultValue: [-50, 0, 100] }))
-      .assertion(slider.has({ orientation: 'horizontal' }))
-      .assertion(slider.has({ value: [-50, 0, 100] }))
-      .assertion(slider.has({ textValue: ['-50°C', '0°C', '100°C'] }))
-      .assertion(slider.has({ minValue: -100 }))
-      .assertion(slider.has({ maxValue: 200 }))
-      .assertion(slider.is({ horizontal: true }))
-      .assertion(slider.is({ vertical: false }))
-      .assertion(slider.is({ min: false }))
-      .assertion(slider.is({ max: false }))
-      .assertion(firstThumb.has({ index: 0, value: -50, textValue: '-50°C' }))
-      .assertion(secondThumb.has({ index: 1, value: 0, textValue: '0°C' }))
-      .assertion(thirdThumb.has({ index: 2, value: 100, textValue: '100°C' }))
-      .assertion(slider.find(Thumb('nth')).absent())
+      .assertion(
+        slider.has({ orientation: 'horizontal' }),
+        slider.has({ value: [-50, 0, 100] }),
+        slider.has({ textValue: ['-50°C', '0°C', '100°C'] }),
+        slider.has({ minValue: -100 }),
+        slider.has({ maxValue: 200 }),
+        slider.is({ horizontal: true }),
+        slider.is({ vertical: false }),
+        slider.is({ min: false }),
+        slider.is({ max: false }),
+        firstThumb.has({ index: 0, value: -50, textValue: '-50°C' }),
+        secondThumb.has({ index: 1, value: 0, textValue: '0°C' }),
+        thirdThumb.has({ index: 2, value: 100, textValue: '100°C' }),
+        slider.find(Thumb('nth')).absent()
+      )
       .child('decrease 2nd thumb', (test) => test
         .step(secondThumb.decrease())
-        .assertion(secondThumb.has({ value: -1 }))
-        .assertion(secondThumb.has({ textValue: '-1°C' }))
+        .assertion(
+          secondThumb.has({ value: -1 }),
+          secondThumb.has({ textValue: '-1°C' })
+        )
       )
       .child('decrease 2nd thumb by 10', (test) => test
         .step(secondThumb.decrease(10))
-        .assertion(secondThumb.has({ value: -10 }))
-        .assertion(secondThumb.has({ textValue: '-10°C' }))
+        .assertion(
+          secondThumb.has({ value: -10 }),
+          secondThumb.has({ textValue: '-10°C' })
+        )
       )
       .child('decrease 2nd thumb by 200', (test) => test
         .step(secondThumb.decrease(200))
-        .assertion(slider.has({ value: [-100, -50, 100] }))
-        .assertion(secondThumb.has({ value: -50 }))
-        .assertion(secondThumb.has({ textValue: '-50°C' }))
-        .assertion(secondThumb.is({ min: false }))
-        .assertion(firstThumb.has({ value: -100 }))
-        .assertion(firstThumb.has({ textValue: '-100°C' }))
-        .assertion(firstThumb.is({ min: true }))
+        .assertion(
+          slider.has({ value: [-100, -50, 100] }),
+          secondThumb.has({ value: -50 }),
+          secondThumb.has({ textValue: '-50°C' }),
+          secondThumb.is({ min: false }),
+          firstThumb.has({ value: -100 }),
+          firstThumb.has({ textValue: '-100°C' }),
+          firstThumb.is({ min: true })
+        )
       )
       .child('increase 2nd thumb', (test) => test
         .step(secondThumb.increase())
-        .assertion(secondThumb.has({ value: 1 }))
-        .assertion(secondThumb.has({ textValue: '1°C' }))
+        .assertion(
+          secondThumb.has({ value: 1 }),
+          secondThumb.has({ textValue: '1°C' })
+        )
       )
       .child('increase 2nd thumb by 10', (test) => test
         .step(secondThumb.increase(10))
-        .assertion(secondThumb.has({ value: 10 }))
-        .assertion(secondThumb.has({ textValue: '10°C' }))
+        .assertion(
+          secondThumb.has({ value: 10 }),
+          secondThumb.has({ textValue: '10°C' })
+        )
       )
       .child('increase 2nd thumb by 300', (test) => test
         .step(secondThumb.increase(300))
-        .assertion(slider.has({ value: [-50, 100, 200] }))
-        .assertion(secondThumb.has({ value: 100 }))
-        .assertion(secondThumb.has({ textValue: '100°C' }))
-        .assertion(secondThumb.is({ max: false }))
-        .assertion(thirdThumb.has({ value: 200 }))
-        .assertion(thirdThumb.has({ textValue: '200°C' }))
-        .assertion(thirdThumb.is({ max: true }))
+        .assertion(
+          slider.has({ value: [-50, 100, 200] }),
+          secondThumb.has({ value: 100 }),
+          secondThumb.has({ textValue: '100°C' }),
+          secondThumb.is({ max: false }),
+          thirdThumb.has({ value: 200 }),
+          thirdThumb.has({ textValue: '200°C' }),
+          thirdThumb.is({ max: true })
+        )
       )
       .child('setMin 2nd thumb', (test) => test
         .step(secondThumb.setMin())
-        .assertion(slider.is({ min: false }))
-        .assertion(secondThumb.is({ min: false }))
-        .assertion(firstThumb.is({ min: true }))
+        .assertion(
+          slider.is({ min: false }),
+          secondThumb.is({ min: false }),
+          firstThumb.is({ min: true })
         )
+      )
       .child('setMin all thumbs', (test) => test
-        .step(firstThumb.setMin())
-        .step(secondThumb.setMin())
-        .step(thirdThumb.setMin())
-        .assertion(slider.is({ min: true }))
-        .assertion(firstThumb.is({ min: true }))
-        .assertion(secondThumb.is({ min: true }))
-        .assertion(thirdThumb.is({ min: true }))
+        .step(
+          firstThumb.setMin(),
+          secondThumb.setMin(),
+          thirdThumb.setMin()
+        )
+        .assertion(
+          slider.is({ min: true }),
+          firstThumb.is({ min: true }),
+          secondThumb.is({ min: true }),
+          thirdThumb.is({ min: true })
+        )
       )
       .child('setMax 2nd thumb', (test) => test
         .step(secondThumb.setMax())
-        .assertion(slider.is({ max: false }))
-        .assertion(secondThumb.is({ max: false }))
-        .assertion(thirdThumb.is({ max: true }))
+        .assertion(
+          slider.is({ max: false }),
+          secondThumb.is({ max: false }),
+          thirdThumb.is({ max: true })
+        )
       )
       .child('setMax all thumbs', (test) => test
-        .step(thirdThumb.setMax())
-        .step(secondThumb.setMax())
-        .step(firstThumb.setMax())
-        .assertion(slider.is({ max: true }))
-        .assertion(firstThumb.is({ max: true }))
-        .assertion(secondThumb.is({ max: true }))
-        .assertion(thirdThumb.is({ max: true }))
+        .step(
+          thirdThumb.setMax(),
+          secondThumb.setMax(),
+          firstThumb.setMax()
+        )
+        .assertion(
+          slider.is({ max: true }),
+          firstThumb.is({ max: true }),
+          secondThumb.is({ max: true }),
+          thirdThumb.is({ max: true })
+        )
       )
       .child('setValue 2nd thumb', (test) => test
         .step(secondThumb.setValue(50))
@@ -355,13 +405,17 @@ export default test("Slider")
       )
       .child('setValue 2nd thumb below min', (test) => test
         .step(secondThumb.setValue(-200))
-        .assertion(secondThumb.has({ value: -50 }))
-        .assertion(firstThumb.has({ value: -100 }))
+        .assertion(
+          secondThumb.has({ value: -50 }),
+          firstThumb.has({ value: -100 })
+        )
       )
       .child('setValue 2nd thumb above max', (test) => test
         .step(secondThumb.setValue(300))
-        .assertion(secondThumb.has({ value: 100 }))
-        .assertion(thirdThumb.has({ value: 200 }))
+        .assertion(
+          secondThumb.has({ value: 100 }),
+          thirdThumb.has({ value: 200 })
+        )
       )
     )
   );
