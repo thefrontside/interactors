02015ab1fac1b900e0530fc35cab327cb015adc1
minkimcello <minkimcello@gmail.com>
2021-08-17 10:29:08 -0400

    Add play fx to accordion example

--- stories/accordion/index.stories.tsx
+++ stories/accordion/index.stories.tsx
@@ -5,6 +5,8 @@ import { CustomizedAccordion } from "./customized";
 import { DetailedAccordion } from "./detailed";
 import { SimpleAccordion } from "./simple";
 
+import { Accordion as AccordionInteractor } from '../../src/accordion';
+
 export default {
   component: Accordion,
   title: "Accordion",
@@ -12,6 +14,10 @@ export default {
 
 export const Simple = {
   render: (): JSX.Element => <SimpleAccordion />,
+  play: async () => {
+    await AccordionInteractor('Accordion 1').toggle();
+    await AccordionInteractor('Accordion 2').toggle();
+  }
 };
 
 export const Controlled = {
