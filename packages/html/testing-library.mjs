559317c2e6a8f9cb54ef16c24544097fd54d35fd
wKich <w@kich.dev>
2021-10-01 08:18:58 +0200

    add es modules fallback for testing-library entry point

---
+++ packages/html/testing-library.mjs
@@ -0,0 +1,3 @@
+// NOTE: Use this file as a fallback for utils those don't support `exports` field in package.json
+/** @type {import('./src/testing-library')} */
+export * from "./dist/esm/testing-library";
