6d3fc6949c09262ec2bd4fe62d92ceb0d436511d
wKich <w@kich.dev>
2022-03-09 16:20:57 +0500

    Update Storybook preview.tsx

--- packages/material-ui/.storybook/preview.tsx
+++ packages/material-ui/.storybook/preview.tsx
@@ -1,20 +1,28 @@
 import { instrument } from "@storybook/instrumenter";
-import { addActionWrapper, addInteractionWrapper, ActionEvent } from "@interactors/globals";
+import { addInteractionWrapper } from "@interactors/globals";
 
 // NOTE: Dummy call to initialize instrumenter. See: https://github.com/storybookjs/storybook/blob/next/lib/instrumenter/src/instrumenter.ts#L512
 instrument({});
 
 let topOperationRef = null;
 
-addInteractionWrapper((interaction, next: () => Promise<T>):  => async () => {
-  if (!topOperationRef) topOperationRef = operation;
+addInteractionWrapper((interaction, next: () => Promise<unknown>) => async () => {
+  if (!topOperationRef) topOperationRef = next;
   try {
-    if (topOperationRef != operation) return operation;
+    if (topOperationRef != next) return next();
+    let result;
+    const cancel = () => interaction.halt()
+    __STORYBOOK_ADDONS_CHANNEL__.once('setCurrentStory', cancel)
     const instrumenter = global.window.__STORYBOOK_ADDON_INTERACTIONS_INSTRUMENTER__;
-    await instrumenter.track(interaction.options.code(), next, [], {
+    await instrumenter.track(interaction.options.code(), async () => {
+      result = await next()
+      return result
+    }, [], {
       intercept: () => true,
     });
+    __STORYBOOK_ADDONS_CHANNEL__.off('setCurrentStory', cancel)
+    return result;
   } finally {
-    if (topOperationRef == operation) topOperationRef = null;
+    if (topOperationRef == next) topOperationRef = null;
   }
 });
