2d44bcba1df03b4fd0a1c680207a63efda8d42ac
Dmitriy Lazarev <w@kich.dev>
2023-08-28 16:38:46 +0400

    Migrate cypress.json to cypress.config.ts
    Signed-off-by: Dmitriy Lazarev <w@kich.dev>

---
+++ packages/with-cypress/cypress.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  video: false,
+  screenshotOnRunFailure: false,
+  defaultCommandTimeout: 20000,
+  fixturesFolder: 'cypress/fixtures',
+  screenshotsFolder: 'cypress/screenshots',
+  videosFolder: 'cypress/videos',
+  configFile: 'cypress/tsconfig.json',
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    baseUrl: 'http://localhost:3000',
+  },
+})
