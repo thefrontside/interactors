170ea355e80d2c369bd1e06393bb97e2c33350c7
Dmitriy Lazarev <w@kich.dev>
2024-04-30 18:32:47 +0400

    Fix material-ui tests and with-cypress
    Signed-off-by: Dmitriy Lazarev <w@kich.dev>

--- packages/with-cypress/cypress.config.js
+++ packages/with-cypress/cypress.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'cypress'
+const { defineConfig } = require('cypress')
 
-export default defineConfig({
+module.exports = defineConfig({
   video: false,
   screenshotOnRunFailure: false,
   defaultCommandTimeout: 20000,
