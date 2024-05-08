f49d1b2eb69a39229edcf4f83959b9e9b1431844
Charles Lowell <cowboyd@frontside.com>
2024-05-08 15:27:30 -0500

    Don't break every input field if there is an aria labelled input
    https://github.com/thefrontside/interactors/issues/254

--- packages/html/test/text-field.test.ts
+++ packages/html/test/text-field.test.ts
@@ -7,7 +7,7 @@ describe('@interactors/html', () => {
   describe('TextField', () => {
     it('finds `input` tags without type by label', async () => {
       dom(`
-        <label for="name-field">Name</label><input id="name-field"/>
+	<label for="name-field">Name</label><input id="name-field"/>
       `);
 
       await expect(TextField('Name').exists()).resolves.toBeUndefined();
@@ -16,7 +16,7 @@ describe('@interactors/html', () => {
 
     it('finds `input[type=text]` tags by label', async () => {
       dom(`
-        <label for="name-field">Name</label><input type="text" id="name-field"/>
+	<label for="name-field">Name</label><input type="text" id="name-field"/>
       `);
 
       await expect(TextField('Name').exists()).resolves.toBeUndefined();
@@ -25,17 +25,24 @@ describe('@interactors/html', () => {
 
     it('finds `textarea` tags by label', async () => {
       dom(`
-        <label for="name-field">Name</label>
-        <textarea id="name-field"></textarea>
+	<label for="name-field">Name</label>
+	<textarea id="name-field"></textarea>
       `);
 
       await expect(TextField('Name').exists()).resolves.toBeUndefined();
       await expect(TextField('Does not Exist').exists()).rejects.toHaveProperty('name', 'NoSuchElementError');
     });
 
+    it('finds `input` tags with an aria-label', async () => {
+      dom(`
+        <input type="text" aria-label="Name">
+`);
+      await expect(TextField("Name").exists()).resolves.toBeUndefined();
+    })
+
     it('finds `input` tags with custom type', async () => {
       dom(`
-        <label for="name-field">Name</label><input type="monkey" id="name-field"/>
+	<label for="name-field">Name</label><input type="monkey" id="name-field"/>
       `);
 
       await expect(TextField('Name').exists()).resolves.toBeUndefined();
@@ -44,7 +51,7 @@ describe('@interactors/html', () => {
 
     it('does not find non text field inputs', async () => {
       dom(`
-        <label for="name-field">Name</label><input type="range" id="name-field"/>
+	<label for="name-field">Name</label><input type="range" id="name-field"/>
       `);
 
       await expect(TextField('Name').exists()).rejects.toHaveProperty('name', 'NoSuchElementError');
@@ -52,9 +59,9 @@ describe('@interactors/html', () => {
 
     it('does not yet support multiple label tags per `input`, becomes rejected as ambiguous', async () => {
       dom(`
-        <label for="name-field">Name</label>
-        <label for="name-field">Designation</label>
-        <input id="name-field"/>
+	<label for="name-field">Name</label>
+	<label for="name-field">Designation</label>
+	<input id="name-field"/>
       `);
 
       await expect(TextField('Name').exists()).rejects.toHaveProperty('name', 'AmbiguousElementError');
@@ -62,168 +69,168 @@ describe('@interactors/html', () => {
 
     describe('.click', () => {
       it('clicks on field', async () => {
-        dom(`
-          <p>
-            <label for="nameField">Name</label>
-            <input type="text" id="nameField"/>
-            <h1 id="target"></h1>
-          </p>
-          <script>
-            nameField.addEventListener('click', (event) => target.textContent = 'Success');
-          </script>
-        `);
+	dom(`
+	  <p>
+	    <label for="nameField">Name</label>
+	    <input type="text" id="nameField"/>
+	    <h1 id="target"></h1>
+	  </p>
+	  <script>
+	    nameField.addEventListener('click', (event) => target.textContent = 'Success');
+	  </script>
+	`);
 
-        await TextField('Name').click();
-        await Heading('Success').exists();
+	await TextField('Name').click();
+	await Heading('Success').exists();
       });
     });
 
     describe('.focus', () => {
       it('focuses on field', async () => {
-        dom(`
-          <p>
-            <label for="nameField">Name</label>
-            <input type="text" id="nameField"/>
-            <h1 id="target"></h1>
-          </p>
-          <script>
-            nameField.addEventListener('focus', (event) => target.textContent = 'Success');
-          </script>
-        `);
+	dom(`
+	  <p>
+	    <label for="nameField">Name</label>
+	    <input type="text" id="nameField"/>
+	    <h1 id="target"></h1>
+	  </p>
+	  <script>
+	    nameField.addEventListener('focus', (event) => target.textContent = 'Success');
+	  </script>
+	`);
 
-        await TextField('Name').focus();
-        await TextField('Name').is({ focused: true });
-        await Heading('Success').exists();
+	await TextField('Name').focus();
+	await TextField('Name').is({ focused: true });
+	await Heading('Success').exists();
       });
     });
 
     describe('.blur', () => {
       it('blurs field', async () => {
-        dom(`
-          <p>
-            <label for="nameField">Name</label>
-            <input type="text" id="nameField"/>
-            <h1 id="target"></h1>
-          </p>
-          <script>
-            nameField.addEventListener('blur', (event) => target.textContent = 'Success');
-          </script>
-        `);
+	dom(`
+	  <p>
+	    <label for="nameField">Name</label>
+	    <input type="text" id="nameField"/>
+	    <h1 id="target"></h1>
+	  </p>
+	  <script>
+	    nameField.addEventListener('blur', (event) => target.textContent = 'Success');
+	  </script>
+	`);
 
-        await TextField('Name').focus();
-        await TextField('Name').blur();
-        await TextField('Name').is({ focused: false });
-        await Heading('Success').exists();
+	await TextField('Name').focus();
+	await TextField('Name').blur();
+	await TextField('Name').is({ focused: false });
+	await Heading('Success').exists();
       });
     });
 
     describe('.fillIn', () => {
       it('fills in the field', async () => {
-        dom(`
-          <p>
-            <label for="nameField">Name</label>
-            <input type="text" id="nameField"/>
-          </p>
-        `);
+	dom(`
+	  <p>
+	    <label for="nameField">Name</label>
+	    <input type="text" id="nameField"/>
+	  </p>
+	`);
 
-        await TextField('Name').fillIn('John');
-        await TextField('Name').has({ value: 'John' });
+	await TextField('Name').fillIn('John');
+	await TextField('Name').has({ value: 'John' });
       });
     });
 
     describe('filter `title`', () => {
       it('filters `input` tags by their title', async () => {
-        dom(`
-          <p>
-            <label for="name-field">Name</label>
-            <input id="name-field" type="text" title="My Name Field"/>
-          </p>
-        `);
+	dom(`
+	  <p>
+	    <label for="name-field">Name</label>
+	    <input id="name-field" type="text" title="My Name Field"/>
+	  </p>
+	`);
 
-        await expect(TextField('Name', { title: 'My Name Field' }).exists()).resolves.toBeUndefined();
-        await expect(TextField('Name', { title: 'Does not exist' }).exists()).rejects.toHaveProperty('name', 'NoSuchElementError');
+	await expect(TextField('Name', { title: 'My Name Field' }).exists()).resolves.toBeUndefined();
+	await expect(TextField('Name', { title: 'Does not exist' }).exists()).rejects.toHaveProperty('name', 'NoSuchElementError');
       });
     });
 
     describe('filter `disabled`', () => {
       it('filters `input` tags by their disabled state', async () => {
-        dom(`
-          <p><input id="name-field" disabled/></p>
-          <p><input id="address-field"/></p>
-        `);
+	dom(`
+	  <p><input id="name-field" disabled/></p>
+	  <p><input id="address-field"/></p>
+	`);
 
-        await expect(TextField({ id: 'name-field', disabled: true }).exists()).resolves.toBeUndefined();
-        await expect(TextField({ id: 'address-field', disabled: false }).exists()).resolves.toBeUndefined();
-        await expect(TextField({ id: 'name-field', disabled: false }).exists()).rejects.toHaveProperty('name', 'NoSuchElementError');
-        await expect(TextField({ id: 'address-field', disabled: true }).exists()).rejects.toHaveProperty('name', 'NoSuchElementError');
+	await expect(TextField({ id: 'name-field', disabled: true }).exists()).resolves.toBeUndefined();
+	await expect(TextField({ id: 'address-field', disabled: false }).exists()).resolves.toBeUndefined();
+	await expect(TextField({ id: 'name-field', disabled: false }).exists()).rejects.toHaveProperty('name', 'NoSuchElementError');
+	await expect(TextField({ id: 'address-field', disabled: true }).exists()).rejects.toHaveProperty('name', 'NoSuchElementError');
       });
 
       it('defaults to only enabled', async () => {
-        dom(`
-          <p><input id="name-field" disabled/></p>
-          <p><input id="address-field"/></p>
-        `);
+	dom(`
+	  <p><input id="name-field" disabled/></p>
+	  <p><input id="address-field"/></p>
+	`);
 
-        await expect(TextField({ id: 'address-field' }).exists()).resolves.toBeUndefined();
-        await expect(TextField({ id: 'name-field' }).exists()).rejects.toHaveProperty('name', 'NoSuchElementError');
+	await expect(TextField({ id: 'address-field' }).exists()).resolves.toBeUndefined();
+	await expect(TextField({ id: 'name-field' }).exists()).rejects.toHaveProperty('name', 'NoSuchElementError');
       });
     });
 
     describe('filter `id`', () => {
       it('filters `input` tags by id', async () => {
-        dom(`
-          <p>
-            <label for="name-field">Name</label>
-            <input id="name-field"/>
-          </p>
-        `);
+	dom(`
+	  <p>
+	    <label for="name-field">Name</label>
+	    <input id="name-field"/>
+	  </p>
+	`);
 
-        await expect(TextField('Name', { id: 'name-field' }).exists()).resolves.toBeUndefined();
-        await expect(TextField('Name', { id: 'does-not-exist' }).exists()).rejects.toHaveProperty('name', 'NoSuchElementError');
+	await expect(TextField('Name', { id: 'name-field' }).exists()).resolves.toBeUndefined();
+	await expect(TextField('Name', { id: 'does-not-exist' }).exists()).rejects.toHaveProperty('name', 'NoSuchElementError');
       });
     });
 
     describe('filter `value`', () => {
       it('filters `input` tags by value', async () => {
-        dom(`
-          <p>
-            <label for="name-field">Name</label>
-            <input id="name-field" value="John"/>
-          </p>
-        `);
+	dom(`
+	  <p>
+	    <label for="name-field">Name</label>
+	    <input id="name-field" value="John"/>
+	  </p>
+	`);
 
-        await expect(TextField('Name', { value: 'John' }).exists()).resolves.toBeUndefined();
-        await expect(TextField('Name', { value: 'Does not Exist' }).exists()).rejects.toHaveProperty('name', 'NoSuchElementError');
+	await expect(TextField('Name', { value: 'John' }).exists()).resolves.toBeUndefined();
+	await expect(TextField('Name', { value: 'Does not Exist' }).exists()).rejects.toHaveProperty('name', 'NoSuchElementError');
       });
     });
 
     describe('filter `placeholder`', () => {
       it('filters `input` tags by placeholder', async () => {
-        dom(`
-          <p>
-            <label for="name-field">Name</label>
-            <input id="name-field" placeholder="Your Name"/>
-          </p>
-        `);
+	dom(`
+	  <p>
+	    <label for="name-field">Name</label>
+	    <input id="name-field" placeholder="Your Name"/>
+	  </p>
+	`);
 
-        await expect(TextField('Name', { placeholder: 'Your Name' }).exists()).resolves.toBeUndefined();
-        await expect(TextField('Name', { placeholder: 'Does not Exist' }).exists()).rejects.toHaveProperty('name', 'NoSuchElementError');
+	await expect(TextField('Name', { placeholder: 'Your Name' }).exists()).resolves.toBeUndefined();
+	await expect(TextField('Name', { placeholder: 'Does not Exist' }).exists()).rejects.toHaveProperty('name', 'NoSuchElementError');
       });
     });
 
     describe('filter `valid`', () => {
       it('filters `input` tags by their validity state', async () => {
-        dom(`
-          <p>
-            <input required id="name-field" value="John"/>
-            <input required id="address-field"/>
-          </p>
-        `);
+	dom(`
+	  <p>
+	    <input required id="name-field" value="John"/>
+	    <input required id="address-field"/>
+	  </p>
+	`);
 
-        await expect(TextField({ id: 'name-field', valid: true }).exists()).resolves.toBeUndefined();
-        await expect(TextField({ id: 'address-field', valid: false }).exists()).resolves.toBeUndefined();
-        await expect(TextField({ id: 'name-field', valid: false }).exists()).rejects.toHaveProperty('name', 'NoSuchElementError');
-        await expect(TextField({ id: 'address-field', valid: true }).exists()).rejects.toHaveProperty('name', 'NoSuchElementError');
+	await expect(TextField({ id: 'name-field', valid: true }).exists()).resolves.toBeUndefined();
+	await expect(TextField({ id: 'address-field', valid: false }).exists()).resolves.toBeUndefined();
+	await expect(TextField({ id: 'name-field', valid: false }).exists()).rejects.toHaveProperty('name', 'NoSuchElementError');
+	await expect(TextField({ id: 'address-field', valid: true }).exists()).rejects.toHaveProperty('name', 'NoSuchElementError');
       });
     });
   });
