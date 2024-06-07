46af29b9e3b74615f37c4fe630dce41393b329e0
Charles Lowell <cowboyd@frontside.com>
2024-06-07 13:41:09 +0300

    Add dev command to watch and incrementally build

--- packages/cli/src/build.ts
+++ packages/cli/src/build.ts
@@ -1,19 +1,26 @@
 import { call, Operation } from "effection";
-import { readFile, writeFile } from "fs/promises";
+import { mkdir, readFile, writeFile } from "node:fs/promises";
+import * as path from "node:path";
 import { generateImports } from "./generate-imports.ts";
 import { importInteractors } from "./import-interactors.ts";
 import { generateConstructors } from "./generate-constructors.ts";
 
 export interface BuildOptions {
   outDir: string;
+  modules?: [];
 }
 export function* build(options: BuildOptions): Operation<void> {
   let outDir = options.outDir;
+
+  yield* call(() => mkdir(outDir, { recursive: true }));
+
+  const modules = modulePaths(options);
 
   let modulesList = new Set([
     // NOTE: Include core by default
     "@interactors/core",
     "@interactors/html",
+    ...modules,
   ]);
 
   let imports: { [moduleName: string]: Record<string, unknown> } = {};
@@ -39,5 +46,11 @@ export function* build(options: BuildOptions): Operation<void> {
   yield* call(
     writeFile(`${outDir}/agent.ts`, [importCode, agentTemplate].join("\n")),
   );
+  console.log(`${outDir}/agent.ts`);
   yield* call(writeFile(`${outDir}/constructors.ts`, constructorsCode));
+  console.log(`${outDir}/constructors.ts`);
+}
+
+export function modulePaths(options: BuildOptions): string[] {
+  return (options.modules ?? []).map((name) => path.resolve(name));
 }
