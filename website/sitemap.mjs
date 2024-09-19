b730c41a3af6291ec7bf805e8ca686c563316409
Charles Lowell <cowboyd@frontside.com>
2024-09-19 09:29:20 -0500

    ✨ Add sitemap.xml to interactors site.
    We're going to use sitemaps as a standards based mechanism to compose
    our static websites.
    
    This adds a sitemap for the interactors website so that it can be
    "mounted" on frontside.com by enabling the docusaurus "sitemap"
    plugin.
    
    Docusaurus does a bunch of dynamic chicanery with React,
    Webpack and code-splitting to render a static webpage because
    reasons, so we add a synthetic entry to the sitemap that serves as an
    index for all dynamic JavaScript resources. That way, the static site generator
    knows to download all those resources ahead of time even though they
    are not referenced in any page.

---
+++ website/sitemap.mjs
@@ -0,0 +1,58 @@
+/* Add indexes of all dynamically loaded assets so that they can be staticalized */
+
+import { readFileSync, writeFileSync, readdirSync } from "node:fs";
+import { Parser, Builder } from "xml2js";
+
+const [parser, builder] = [new Parser(), new Builder()];
+
+const sitemapXML = readFileSync("./build/interactors/sitemap.xml");
+
+const sitemap = await parser.parseStringPromise(sitemapXML);
+
+// ideally we would use the docusaurs sitemap plugin to do this
+// but support for plugins came with => 3.0.0 and upgrade no thank you.
+sitemap.urlset.url.push({
+  loc: 'https://interactors.deno.dev/interactors/assets/js/index.html',
+}, {
+  loc: 'https://interactors.deno.dev/interactors/images/index.html'
+});
+
+writeFileSync("./build/interactors/sitemap.xml", builder.buildObject(sitemap));
+
+const assets = readdirSync("./build/interactors/assets/js");
+
+writeFileSync("./build/interactors/assets/js/index.html",`
+<html>
+  <head>
+${assets.map((asset) => `    <link rel="asset" href="${asset}"/>`).join("\n")}
+  </head>
+  <body>
+  <h1> JavaScript Index</h1>
+  <ul>
+
+${assets.map((asset) => `    <li><a href="${asset}">${asset}</a></li>`).join("\n")}
+
+  </ul>
+  <body>
+<html>
+
+`);
+
+const images = readdirSync("./build/interactors/images");
+
+writeFileSync("./build/interactors/images/index.html",`
+<html>
+  <head>
+${images.map((image) => `    <link rel="asset" href="${image}"/>`).join("\n")}
+  </head>
+  <body>
+  <h1> JavaScript Index</h1>
+  <ul>
+
+${images.map((image) => `    <li><a href="${image}">${image}</a></li>`).join("\n")}
+
+  </ul>
+  <body>
+<html>
+
+`);
